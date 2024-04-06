bc2d5a941e522586376e35afbed8fff9d8f4450a
YukiTakada(髙田雄樹) <yuki.takada.s523@dirbato.co.jp>
2024-04-06 21:49:05 +0900

    条件分岐２

--- Sample/src/sample/IfSample.java
+++ Sample/src/sample/IfSample.java
@@ -3,13 +3,18 @@ package sample;
 public class IfSample {
 
     public static void main(String[] args) {
-        var score = -10; // 成績
+        var score = 100; // 成績
         
-        if (score >= 60) {
+        if (score == 100) {//=が２個ないと値の格納になってしまう
+            System.out.println("満点です！");
+        } else if (score >= 80) {
+            System.out.println("よくできました！");
+        } else if (score >=60 ) {
             System.out.println("合格です！");
         } else {
-            System.out.println("赤点です、、、");
-        }
+            System.out.println("赤点です。。。");
+            }
+        
 
     }
 

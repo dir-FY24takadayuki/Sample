b94db6e9c76771e35c511c05525d0bb4f2a3b6f4
YukiTakada(髙田雄樹) <yuki.takada.s523@dirbato.co.jp>
2024-04-06 22:08:24 +0900

    奇数ならおっど 偶数ならイーブン

---
+++ Sample/src/sample/freepractice.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class freepractice {
+
+    public static void main(String[] args) {
+        int n = 33;
+        if (n % 2 == 1) {
+            System.out.println("odd"); 
+        } else {
+            System.out.println("even");
+        }
+                
+
+    }
+
+}

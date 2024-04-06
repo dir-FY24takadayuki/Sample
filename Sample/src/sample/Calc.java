793387b8ef591ae9a4352a802fabaf7314314d52
YukiTakada(髙田雄樹) <yuki.takada.s523@dirbato.co.jp>
2024-04-06 17:07:00 +0900

    Chapter 5.1　例題

---
+++ Sample/src/sample/Calc.java
@@ -0,0 +1,29 @@
+package sample;
+
+import java.security.PublicKey;
+
+public class Calc {
+
+    public static void main(String[] args) {
+        var num1 = 100;
+        var num2 = 10;
+        
+        //足し算
+        int result6 = num1 + num2;
+        System.out.println("num1 + num2 = " + result6);
+        
+        //引き算
+        var result2 = num1 - num2;
+        System.out.println("num1 + num2 = " + result2);
+        
+        //掛け算
+        var result3 = num1 * num2;
+        System.out.println("num1 * num2 = " + result3);
+        
+        //割り算
+        var result5 = num1 % num2;
+        System.out.println("num1 % num2 = " +result5);
+
+    }
+
+}
--- Sample/src/sample/practice_myself.java
+++ Sample/src/sample/practice_myself.java
@@ -2,13 +2,7 @@ package sample;
 
 public class practice_myself {
 
-    public static void main(String[] args) {
-        //一日の秒数＝２４時間×６０分×６０秒
-        var secondsPerDay = 24 * 60 * 60; // *は掛け算を意味する記号
-        
-        System.out.println(secondsPerDay);
+    
         
 
     }
-
-}

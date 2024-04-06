751bc2ffad7f90d9ad2e375c41d0258c5b20e877
YukiTakada(髙田雄樹) <yuki.takada.s523@dirbato.co.jp>
2024-04-06 23:13:07 +0900

    loop構造

---
+++ Sample/src/sample/ForSample.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class ForSample {
+    
+    public static void main(String[] args) {
+        for (var i = 1; i <= 10; i++) {//初期化、判定、変更というループ構造の本質が詰まっている
+            System.out.println(i);
+        }
+    }
+
+}
